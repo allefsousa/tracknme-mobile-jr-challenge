4b5ec28f0b4d6592eb39fb4c285cc4ca8b52918a
Allef Sousa <allefsousa_1@hotmail.com>
2017-12-14 09:47:52 -0200

    ViewPrincipalOk

---
+++ app/src/main/java/com/developer/allef/desafiotracknme/View/MenuActivity.java
@@ -0,0 +1,43 @@
+package com.developer.allef.desafiotracknme.View;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.developer.allef.desafiotracknme.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    //region BottomNavigation
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+
+                case R.id.navigation_abrirmapa:
+                    mTextMessage.setText(R.string.openmap);
+                    return true;
+            }
+            return false;
+        }
+    };
+    //endregion
+
+}
